9d18d25f4de85c22a0d67df089c74eeadfa7bee6
James Chemobo <jkchemobo@gmail.com>
2022-06-27 18:42:31 +0300

    commit tree 7f4f32b68744309c71ed1e1903c68b28bd631940

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData = function(userName, userEmail) {
+    const userInfo = {
+        name: userName,
+        email: userEmail
+    };
+        return fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        }).then((response) => {
+            return response.json();
+        }).then((jsonData) => {
+            return document.body.innerHTML = jsonData.id
+        }).catch((err) => {
+            document.body.innerHTML = err.message;
+        });
+    
+    } 
+    submitData('Edilsberg', 'edilsberg@gmail.com');
